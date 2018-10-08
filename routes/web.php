9039720fbe70ea8293c8b8af503542b0b6f6a374
Vlad <coolsword97@gmail.com>
2018-10-08 19:40:01 +0500

    'blog'

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+Route::get('/', function () {
+    return view('blog.index');
+});
+
+Route::get('/dashboard/index', function () {
+    return view('dashboard.index');
+});
+
+Route::get('/dashboard/signin', function () {
+    return view('dashboard.signin');
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
